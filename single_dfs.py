0c294c154d190b05471671eee02534860c5bc429
Aaron Liu <ajliu16@earlham.edu>
2020-01-29 13:30:23 -0500

    fixed starting point bug and implemented DFS

--- single_dfs.py
+++ single_dfs.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import deque
 #hash table to store traveled
 #traveled_set[00] = True
 #example: If examining north:
@@ -9,28 +10,87 @@ class Node():
     def __init__(self, status="0", parent=None, ):
         self.status = status
         self.parent = parent
+        self.traveled = False
+class Maze():
+    def __init__(self,maze,row_length,column_length):
+        self.m = maze
+        self.row_length = row_length
+        self.column_length = column_length
 
-#New function that takes in a maze and a starting point and performs DFS 
-#def dfs(node, maze):
+#New function that takes in a maze and a starting point and performs a search 
+def dfs(position, maze):
+    if position[0]-1 >= 0 and maze.m[position[0]-1][position[1]].status != "%" and maze.m[position[0]-1][position[1]].traveled == False:
+        if maze.m[position[0]-1][position[1]].status ==".":
+            return "found"
+        maze.m[position[0]-1][position[1]].traveled = True
+        maze.m[position[0]-1][position[1]].parent = maze.m[position[0]][position[1]]
+        maze.m[position[0]-1][position[1]].status = "#"
+        return [position[0]-1,position[1]]
+    elif position[1]+1 <= maze.row_length-1 and maze.m[position[0]][position[1]+1].status != "%" and maze.m[position[0]][position[1]+1].traveled == False:
+        if maze.m[position[0]][position[1]+1].status ==".":
+            return "found"
+        maze.m[position[0]][position[1]+1].traveled = True
+        maze.m[position[0]][position[1]+1].parent = maze.m[position[0]][position[1]]
+        maze.m[position[0]][position[1]+1].status = "#"
+        return [position[0],position[1]+1]
+    elif position[0]+1 <= maze.column_length-1 and maze.m[position[0]+1][position[1]].status != "%" and maze.m[position[0]+1][position[1]].traveled == False:
+        if maze.m[position[0]+1][position[1]].status ==".":
+            return "found"
+        maze.m[position[0]+1][position[1]].traveled = True
+        maze.m[position[0]+1][position[1]].parent = maze.m[position[0]][position[1]]
+        maze.m[position[0]+1][position[1]].status = "#"
+        return [position[0]+1,position[1]]
+    elif position[1]-1 >= 0 and maze.m[position[0]][position[1]-1].status != "%" and maze.m[position[0]][position[1]-1].traveled == False:
+        if maze.m[position[0]][position[1]-1].status ==".":
+            return "found"
+        maze.m[position[0]][position[1]-1].traveled = True
+        maze.m[position[0]][position[1]-1].parent = maze.m[position[0]][position[1]]
+        maze.m[position[0]][position[1]-1].status = "#"
+        return [position[0],position[1]-1]
+    else:
+        return False
 def single_dfs(file_location):
-    file = open(f"{file_location}", "r")
-    row_length = len(file.readline())-1
-    column_length = len(file.readlines())+1
+    file = open(f"{file_location}", "r")    
+    row_length = len(file.readline())-1 #more concise than for loop
+    column_length = len(file.readlines())+1 #more concise than for loop
     file.seek(0)
-    maze = [[0 for x in range(row_length)] for y in range(column_length)]
+    maze = Maze([[0 for x in range(row_length)] for y in range(column_length)], row_length, column_length)
     y = 0
+    prizes = []
     for i in file.readlines():
         x = -1
         for f in i:
             if f == "P":
-                sp = [y,x]
+                sp = [y,x+1]
+            if f == ".":
+                prizes.append([y,x])
             if f != "\n":
                 x+=1
-                maze[y][x] = Node(f)
+                maze.m[y][x] = Node(f)
         y+=1
-    our_stack = [maze[sp[0]][sp[1]]]
+
+    #DFS uses a stack LIFO
+    our_stack = [[sp[0],sp[1]]]
+    maze.m[sp[0]][sp[1]].traveled = True
+    # for i in maze.m:
+    #     x = []
+    #     for f in i:
+    #         x.append(f.status)
+    #     print(x)
+
     while our_stack != []:
-        break
+        transition = dfs(our_stack[len(our_stack)-1], maze)
+        if transition:
+            if transition == "found": #found prize
+                break
+            our_stack.append(transition)
+        else:
+            our_stack.pop()
+    for i in maze.m:
+        x = ""
+        for f in i:
+            x+=f.status
+        print(x)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Takes in maze file location and outputs")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+    # for i in maze.m:
+    #     x = []
+    #     for f in i:
+    #         x.append(f.status)
+    #     print(x)
